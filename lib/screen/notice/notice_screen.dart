de617cfb7cabd2b62b2fb54a4d6ddd643e6068d0
ymurillo89 <murilloruiz91@gmail.com>
2023-10-31 21:03:34 -0500

    Commit

---
+++ lib/screen/notice/notice_screen.dart
@@ -0,0 +1,41 @@
+import 'package:appuenvigado/screen/home/home_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NoticeScreen extends StatefulWidget {
+  const NoticeScreen({super.key});
+
+  @override
+  State<NoticeScreen> createState() => _NoticeScreenState();
+}
+
+class _NoticeScreenState extends State<NoticeScreen> {
+
+  HomeProvider homeProvider = HomeProvider();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Notificaciones"),
+
+      ),
+    );
+  }
+  
+    Widget image(){
+      if(homeProvider.image.isEmpty)
+      {
+        return Center(child: Text("Notificaciones"));
+      }else{
+
+        return Column(
+
+        );
+
+
+
+      }
+    }
+}
